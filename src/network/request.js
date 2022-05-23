71d3e0537a8227bd048f0fbc83e60d8eba6a2272
ZackChang <997571692@qq.com>
2022-05-24 01:16:04 +0800

    chore: 更新接口

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   // 新建一个axios实例
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:7878/api/m5',
+    baseURL: 'http://152.136.185.210:7878/api/hy66',
     timeout: 5000
   })
   return instance(config)
